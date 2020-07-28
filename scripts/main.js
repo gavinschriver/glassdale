4adff2ce6b755c88af117f6c8415977fbbcd3fcf
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-27 21:28:34 -0500

    added

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,11 @@ import { ConvictionSelect} from "./convictions/ConvictionSelect.js"
 import { getCriminals, useCriminals} from "./criminals/CriminalProvider.js";
 import { getConvictions, useConvictions} from "./convictions/ConvictionProvider.js";
 import { getOfficers, useOfficers } from "./officers/OfficerProvider.js";
+import { OfficerSelect } from "./officers/OfficerSelect.js";
 
 CriminalList()
 ConvictionSelect()
-
+OfficerSelect()
 
 
 //log out incoming data as logs:
