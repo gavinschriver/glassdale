3c589a6432bb9e515ae2382920211cd1c96c995f
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-29 19:23:24 -0500

    criminal list controls file added

---
+++ scripts/criminals/criminalListControls.js
@@ -0,0 +1,6 @@
+import { ConvictionSelect } from "../convictions/ConvictionSelect.js";
+import { OfficerSelect } from "../officers/OfficerSelect.js";
+import { hideCriminalsButton } from "./hideCriminalListButton.js";
+import { showAllCriminalsButton } from "./showAllCriminalsButton.js";
+
+
