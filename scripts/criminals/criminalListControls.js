c0d8ddf4293ebaa25dcef755f08fd1d36808ba02
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-30 10:53:39 -0500

    total control

--- scripts/criminals/criminalListControls.js
+++ scripts/criminals/criminalListControls.js
@@ -2,18 +2,18 @@ import {showAllCriminalsButton } from "./showAllCriminalsButton.js"
 import { hideCriminalsButton } from "./hideCriminalListButton.js"
 import { OfficerSelect} from "../officers/OfficerSelect.js"
 
-const contentTarget = document.querySelector(".siteHeader")
+const contentTarget = document.querySelector(".criminalListControls")
 
 
 
 export const criminalListControls = () => {
-     let allCriminalControls = 
-        showAllCriminalsButton() + 
-        hideCriminalsButton() +
+     let criminalButtons = showAllCriminalsButton() + hideCriminalsButton()
 
-        OfficerSelect().then(officerDropDown => {
-           allCriminalControls += officerDropDown
-           contentTarget.innerHTML = allCriminalControls
+      OfficerSelect().then(officerDropDown => {
+           criminalButtons += officerDropDown
+           contentTarget.innerHTML = 
+            `<h2>Find Criminals:</h2>` +
+            criminalButtons
 
         })
 
