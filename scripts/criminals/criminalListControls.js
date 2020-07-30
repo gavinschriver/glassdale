d40d9d2edd328b798978d705ca0df15bdb6145a0
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-29 20:28:58 -0500

    uhhhh

--- scripts/criminals/criminalListControls.js
+++ scripts/criminals/criminalListControls.js
@@ -1,9 +1,12 @@
 import {showAllCriminalsButton } from "./showAllCriminalsButton.js"
+import { hideCriminalsButton } from "./hideCriminalListButton.js"
+
 
 const contentTarget = document.querySelector(".siteHeader")
 
 export const criminalListControls = () => {
-     const allCriminalControls = showAllCriminalsButton()
+     const allCriminalControls = 
+        showAllCriminalsButton() + hideCriminalsButton()
 
      contentTarget.innerHTML = allCriminalControls
 }
